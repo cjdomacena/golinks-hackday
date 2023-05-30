e6ff8b58a5a017a1c5777599a8e49154682e2fc6
Cj Domacena <cjdomacena@yahoo.com>
2023-05-30 15:33:27 -0400

    added repo homepage

---
+++ src/components/cards/RepoCard.tsx
@@ -0,0 +1,39 @@
+import { OrgRepo } from "../../lib/types";
+import { AiFillStar, AiOutlineFork } from 'react-icons/ai';
+import { ReactNode } from "react";
+import { Link } from "react-router-dom";
+type RepoCardProps = OrgRepo;
+
+const RepoBadge = ({ text, icon }: { text: string, icon: ReactNode; }) => {
+    return <div className="flex items-center gap-1">
+        {icon}
+        <p>{text}</p>
+    </div>;
+};
+
+export const RepoCard = ({ full_name, description, stargazers_count, forks_count, created_at, owner, name, default_branch }: RepoCardProps) => {
+
+    return <div className="bg-slate-800 p-4 rounded flex flex-col justify-between gap-6">
+        <div className="space-y-3">
+            <div className="flex gap-4">
+                <RepoBadge icon={<AiFillStar />} text={String(stargazers_count)} />
+                <RepoBadge icon={<AiOutlineFork />} text={String(forks_count)} />
+            </div>
+            <Link to={`${owner.login}/${name}/${default_branch}`}><h4 className="text-lg font-bold leading-relaxed mt-2">{full_name}</h4></Link>
+            <p className="line-clamp-3 text-sm text-neutral-300">{description}</p>
+        </div>
+
+
+        <p>{new Intl.DateTimeFormat("en-US", { dateStyle: "medium" }).format(new Date(created_at))}</p>
+
+    </div>;
+
+};
+
+export const RepoCardLoading = () => {
+
+    return <div className="w-full h-[200px] bg-slate-800 animate-pulse rounded">
+
+
+    </div>;
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,65 @@
+import { useQuery } from "@tanstack/react-query";
+import { OrgRepo } from "../lib/types";
+import { BASE_URL } from "../lib/constants";
+import { RepoCard, RepoCardLoading } from "../components/cards/RepoCard";
+import { useState } from "react";
+import { BsGrid, BsList } from 'react-icons/bs';
+
+
+const fetchOrgRepos = async (): Promise<OrgRepo[]> => {
+
+    const res = await fetch(`${BASE_URL}/orgs/Netflix/repos?per_page=20`);
+    return res.json();
+
+};
+
+export default function Home() {
+
+    const { data, status } = useQuery({ queryKey: ["netflix"], queryFn: fetchOrgRepos });
+    const [orientation, setOrientation] = useState<"grid" | "list">("grid");
+
+    // const sortedData = data?.sort((a, b) => a.stargazers_count - b.stargazers_count);
+    const sortedData = data;
+
+    const handleChangeOrientation = (type: "grid" | "list") => {
+        setOrientation(type);
+    };
+
+    switch (status) {
+        case "loading":
+            return <div className="container mx-auto p-4  mt-12 space-y-4">
+                <div className="flex justify-end">
+                    <div className="flex items-center">
+                        <button onClick={() => handleChangeOrientation("grid")} className={`p-2 ${orientation === "grid" ? "bg-neutral-800" : "bg-neutral-700"}`}><BsGrid /></button>
+                        <button onClick={() => handleChangeOrientation("list")} className={`p-2 ${orientation === "list" ? "bg-neutral-800" : "bg-neutral-700"}`}><BsList /></button>
+                    </div>
+                </div>
+                <div>
+                    <h1 className="text-xl font-bold">Netflix Repositories</h1>
+                </div>
+                <div className={`grid ${orientation === "grid" ? "grid-cols-[repeat(auto-fill,minmax(280px,1fr))]" : "grid-cols-1"}  gap-4`}>
+                    {new Array(10).fill(0).map((_, index) => <RepoCardLoading key={`repo-loader-${index}`} />)}
+                </div>
+            </div>;
+        case "error":
+            return <div>Something went wrong...</div>;
+        case "success":
+            return <div className="container mx-auto p-4  mt-12 space-y-4">
+
+                <div className="flex justify-end">
+                    <div className="flex items-center">
+                        <button onClick={() => handleChangeOrientation("grid")} className={`p-2 ${orientation === "grid" ? "bg-slate-800" : "bg-slate-700"}`}><BsGrid /></button>
+                        <button onClick={() => handleChangeOrientation("list")} className={`p-2 ${orientation === "list" ? "bg-slate-800" : "bg-slate-700"}`}><BsList /></button>
+                    </div>
+                </div>
+                <div>
+                    <h1 className="text-xl font-bold">Netflix Repositories</h1>
+                </div>
+                <div className={`grid ${orientation === "grid" ? "grid-cols-[repeat(auto-fill,minmax(280px,1fr))]" : "grid-cols-1"}  gap-4`}>
+                    {sortedData ? sortedData.map((repo) => <RepoCard {...repo} key={repo.id} />) : <p>No repositories available.</p>}
+                </div>
+
+            </div>;
+    }
+    return <div></div>;
+}
